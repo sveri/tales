76f809785e134f83120ab0eb235569ffc91d32d3
sveri <sveri80@gmail.com>
2015-05-31 10:21:57 +0200

    initial commit

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject tales "0.1.0"
+            :description "Source for fales blog"
+            :url "https://github.com/sveri/tales"
+            :license {:name "Eclipse Public License"
+                      :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :dependencies [[org.clojure/clojure "1.6.0"]
+                           [ring/ring-devel "1.3.2"]
+                           [compojure "1.3.4"]
+                           [ring-server "0.4.0"]
+                           [cryogen-markdown "0.1.1"]
+                           [cryogen-core "0.1.21"]]
+            :plugins [[lein-ring "0.8.13"]]
+            :main cryogen.core
+            :ring {:init cryogen.server/init
+                   :handler cryogen.server/handler})
---
+++ resources/templates/md/posts/2015-05-31-first-post.md
@@ -0,0 +1,8 @@
+{:title "First Post"
+ :layout :post
+ :tags  ["cryogen new blog"]}
+
+### First Post in my new blog
+
+I got tired of wordpress and so decided to play around with [cryogen](https://github.com/cryogen-project/cryogen)
+ for static site generation. My old blog of course will be available as usual under <http://blog.sveri.de>.
